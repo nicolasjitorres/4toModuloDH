6b0ac958ee88ecb02d18be71ff518204bd5cc286
Nicolas Torres <nicoignacio123@gmail.com>
2023-11-11 16:38:28 -0300

    Agregando formularios, agregando register y login

--- MercadoLiebre/app.js
+++ MercadoLiebre/app.js
@@ -3,13 +3,24 @@ const path = require('path');
 const app = express();
 const PUERTO = 3000;
 
+app.use(express.static("public")); //Se utiliza para obtener archivos estaticos como pueden ser imagenes o estilos, a traves de una ruta especifica.
 
 app.listen(PUERTO, () => {
     console.log(`El servidor está escuchando en el puerto ${PUERTO}...`);
 });
 
+// HOME
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
 
-app.use(express.static("public")); //Se utiliza para obtener archivos estaticos como pueden ser imagenes o estilos, a traves de una ruta especifica.
+// REGISTER
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+// LOGIN
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
